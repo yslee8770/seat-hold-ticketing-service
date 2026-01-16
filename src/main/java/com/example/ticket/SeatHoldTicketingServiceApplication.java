738bfc724d91442c780fe08e6eb5a2f11aa6abac
yslee <mocuri8770@gmail.com>
2026-01-16 09:16:50 +0900

    EnableJpaAuditing 추가

--- src/main/java/com/example/ticket/SeatHoldTicketingServiceApplication.java
+++ src/main/java/com/example/ticket/SeatHoldTicketingServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.ticket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SeatHoldTicketingServiceApplication {
 
 	public static void main(String[] args) {
